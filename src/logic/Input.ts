6660bcaba3a287b6c7f6865f3444b2f91976c16d
anthonygabriele <anthonyjoeseph@gmail.com>
2021-01-03 06:24:34 -0500

    no timer unless there's input

--- src/logic/Input.ts
+++ src/logic/Input.ts
@@ -22,5 +22,5 @@ export const input = (
         ),
       ),
     ),
-    moveEachFrame,
+    moveEachFrame(state$),
   )
--- src/logic/moveEachFrame.ts
+++ src/logic/moveEachFrame.ts
@@ -1,13 +1,23 @@
 import { Endomorphism, pipe } from 'fp-ts/function'
+import * as S from 'graphics-ts/lib/Shape'
 import * as r from 'rxjs'
 import * as ro from 'rxjs/operators'
 import { GameObject, move } from './GameObject'
 import { frameDeltaMillis$ } from 'game-ts/dist/Render'
 import { windowRect$ } from 'game-ts/dist/Window'
 
-export const moveEachFrame: r.Observable<Endomorphism<GameObject>> = pipe(
-  frameDeltaMillis$,
-  ro.observeOn(r.asyncScheduler),
-  ro.withLatestFrom(windowRect$),
-  ro.map(([delta, rect]) => move(delta, rect)),
-)
+export const moveEachFrame = (
+  state$: r.Observable<GameObject>,
+): r.Observable<Endomorphism<GameObject>> =>
+  pipe(
+    state$,
+    ro.map((s) => !(s.velocity.x === 0 && s.velocity.y === 0)),
+    ro.withLatestFrom(windowRect$),
+    ro.distinctUntilChanged(),
+    ro.switchMap(([moving, rect]) =>
+      pipe(
+        r.iif(() => moving, frameDeltaMillis$, r.EMPTY),
+        ro.map((delta) => move(delta, rect)),
+      ),
+    ),
+  )
